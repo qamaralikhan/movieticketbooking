6b34d9c0396ac4d575698b6eedf19cf5052fad51
qamarali <>
2022-06-15 22:02:12 -0500

    Added new components

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,14 +4,37 @@ const App =()=>
 {
     return(
     <>
-    <nav className='navbar navbar-inverse navbar-fixed-top'>
-    <div className='container'>
-        <div className='navbar-header'>
-        
-       <h3 className='nav-brand'>Book Ticket Now</h3>
+ <div>
+ <form class="form-inline" align="right">
+        <div class="form-group">
+          <input type="text" class="form-control" maxLength="25" placeholder="Search"></input>
+          &nbsp;&nbsp;
         </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </form>
+ </div>
+<nav className="navbar label-info">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">
+      Book My Ticket</a>
     </div>
-    </nav>
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li className="active"><a href="#">Latest Movies <span className="sr-only">(current)</span></a></li>
+        <li><a href="#">Upcoming Movies</a></li>       
+        <li><a href="#">Nearby Event</a></li> 
+      </ul>      
+    </div>
+  </div>
+</nav>
     </>)
 }
 
